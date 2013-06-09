aab7eb48ef71a825f55e7b4ceb3c0d137e8e396c
Richard King <rkingucla@ymail.com>
2013-06-09 07:50:33 -0700

    take into account if no times avail

--- app/models/check.rb
+++ app/models/check.rb
@@ -5,8 +5,8 @@ class Check < ActiveRecord::Base
 
   def check_time
     parsed = Nokogiri::HTML(open(url))
-    times = parsed.css('ul.ResultTimes li').map { |v| Time.strptime(v.to_s.match(/(\d*\/.*[A|P]M)\'\,/)[1], "%m/%d/%Y %I:%M:%S %p") }
-    times.select! {|t| t >= start_time.in_time_zone("Pacific Time (US & Canada)") && t <= end_time.in_time_zone("Pacific Time (US & Canada)") }
+    times = parsed.css('ul.ResultTimes li').map { |v| Time.strptime(v.to_s.match(/(\d*\/.*[A|P]M)\'\,/)[1], "%m/%d/%Y %I:%M:%S %p") rescue nil}
+    times.compact!.select! {|t| t >= start_time.in_time_zone("Pacific Time (US & Canada)") && t <= end_time.in_time_zone("Pacific Time (US & Canada)") }
     times
   end
 end
