14f08b72d83dc921ac8783e4bf812ad5673481b1
Richard King <rkingucla@ymail.com>
2013-06-09 00:40:36 -0700

    finish cap configs

---
+++ config/unicorn.rb
@@ -0,0 +1,9 @@
+root = "/home/deploy/apps/reserve/current"
+working_directory root
+pid "#{root}/tmp/pids/unicorn.pid"
+stderr_path "#{root}/log/unicorn.log"
+stdout_path "#{root}/log/unicorn.log"
+
+listen "/tmp/unicorn.reserve.sock"
+worker_processes 2
+timeout 30
