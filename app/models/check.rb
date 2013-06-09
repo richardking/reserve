b63acc9c81c9020de329ea90bb57e6d1534af0a0
Richard King <rkingucla@ymail.com>
2013-06-09 09:52:57 -0700

    fix compact in check.rb

--- app/models/check.rb
+++ app/models/check.rb
@@ -6,7 +6,7 @@ class Check < ActiveRecord::Base
   def check_time
     parsed = Nokogiri::HTML(open(url))
     times = parsed.css('ul.ResultTimes li').map { |v| Time.strptime(v.to_s.match(/(\d*\/.*[A|P]M)\'\,/)[1], "%m/%d/%Y %I:%M:%S %p") rescue nil}
-    if times.compact!.present?
+    if times = times.compact.present?
       times.select! {|t| t >= start_time.in_time_zone("Pacific Time (US & Canada)") && t <= end_time.in_time_zone("Pacific Time (US & Canada)") }
       return times
     else
