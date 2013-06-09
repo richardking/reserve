0f84289d0a99f46fe144780bb47643f5f24cec9b
Richard King <rkingucla@ymail.com>
2013-06-09 09:42:55 -0700

    add url to mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,8 +2,9 @@ class UserMailer < ActionMailer::Base
   default from: "from@example.com"
 
   def availability_email(check_id, times)
-    @times = times
     check = Check.find(check_id)
+    @times = times
+    @url = check.url
     mail(:to => check.email, :subject => "Test")
   end
 end
