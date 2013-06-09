458e87953eb1f07f2e88f6217a51bbe5e00b9fd7
Richard King <rkingucla@ymail.com>
2013-06-09 09:37:24 -0700

    fix check.rb for empty array

--- app/models/check.rb
+++ app/models/check.rb
@@ -6,7 +6,11 @@ class Check < ActiveRecord::Base
   def check_time
     parsed = Nokogiri::HTML(open(url))
     times = parsed.css('ul.ResultTimes li').map { |v| Time.strptime(v.to_s.match(/(\d*\/.*[A|P]M)\'\,/)[1], "%m/%d/%Y %I:%M:%S %p") rescue nil}
-    times.compact!.select! {|t| t >= start_time.in_time_zone("Pacific Time (US & Canada)") && t <= end_time.in_time_zone("Pacific Time (US & Canada)") }
-    times
+    if times.compact!.present?
+      times.select! {|t| t >= start_time.in_time_zone("Pacific Time (US & Canada)") && t <= end_time.in_time_zone("Pacific Time (US & Canada)") }
+      return times
+    else
+      return []
+    end
   end
 end
