67e72824841110832b4fce43cc346aa96e301120
Richard King <rkingucla@ymail.com>
2013-06-10 15:48:49 -0700

    check only times today or later

--- app/workers/check_time.rb
+++ app/workers/check_time.rb
@@ -2,7 +2,7 @@ class CheckTime
   @queue = :availability
 
   def self.perform
-    Check.all.each do |check|
+    Check.find(:all, :conditions => "Date(start_time) >= Date(NOW())").each do |check|
       times = Check.find(check.id).check_time
       if times.present?
         UserMailer.availability_email(check.id, times).deliver!
