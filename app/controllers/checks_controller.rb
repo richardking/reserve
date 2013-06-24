86a2e52319d83d0dd3a75641be56f4eb57dbb65d
Richard King <rkingucla@ymail.com>
2013-06-24 11:18:09 -0700

    add delete button

--- app/controllers/checks_controller.rb
+++ app/controllers/checks_controller.rb
@@ -3,4 +3,10 @@ class ChecksController < ApplicationController
     @check = Check.create(params[:check])
     redirect_to :root
   end
+
+  def destroy
+    Check.find(params[:id]).destroy
+    redirect_to :root, info: "Succesfully deleted"
+  end
+
 end
